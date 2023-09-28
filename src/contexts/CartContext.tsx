e07f22fe119c895f796509799e14dcdc30b9f528
Ivan Bertucci Reis <ibreis1@stefanini.com>
2023-09-28 11:05:38 -0300

    localStore

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -28,9 +28,25 @@ interface CartProviderProps {
 
 export const CartContext = createContext({} as CartContextProps)
 
+const localStorageKey = "@eCommerceSp:Cart";
+
 export function CartProvider({ children }: CartProviderProps) {
   const navegate = useNavigate()
-  const [cart, setCart] = useState<Snack[]>([])
+  const [cart, setCart] = useState<Snack[]>(() => {
+    const value = localStorage.getItem(localStorageKey)
+    
+    if(value) return JSON.parse(value)
+    return []
+  })
+
+  function saveCart(items: Snack[]){
+    setCart(items)        
+    localStorage.setItem(localStorageKey, JSON.stringify(items))
+  }
+
+  function clearCart() {
+    localStorage.removeItem(localStorageKey)
+  }
 
   function addSnackIntoCart(snack: SnackData): void {
     const snackExistentInCart = cart.find(
@@ -50,7 +66,7 @@ export function CartProvider({ children }: CartProviderProps) {
       })
 
       toast.success(`Outro(a) ${snackEmoji(snack.snack)} ${snack.name} adicionado nos pedidos!`)
-      setCart(newCart)
+      saveCart(newCart)
 
       return
     }
@@ -59,13 +75,13 @@ export function CartProvider({ children }: CartProviderProps) {
     const newCart = [...cart, newSnack] // push de um array
 
     toast.success(`${snackEmoji(snack.snack)} ${snack.name} adicionado nos pedidos!`)
-    setCart(newCart)
+    saveCart(newCart)
   }
 
   function removeSnackFromCart(snack: Snack) {
     const newCart = cart.filter((item) => !(item.id === snack.id && item.snack === snack.snack))
 
-    setCart(newCart)
+    saveCart(newCart)
   }
 
   function updateSnackQuantity(snack: Snack, newQuantity: number) {
@@ -89,7 +105,7 @@ export function CartProvider({ children }: CartProviderProps) {
       return item
     })
 
-    setCart(newCart)
+    saveCart(newCart)
   }
 
   function snackCartIncrement(snack: Snack) {
@@ -105,7 +121,9 @@ export function CartProvider({ children }: CartProviderProps) {
   }
 
   function payOrder(customer: CustomerData) {
-    console.log('payOrder', cart, customer)
+    console.log('payOrder', cart, customer) // chamanda para API Back-End
+    clearCart() // deve ser excutado apos retorno positivo da api
+
     return
   }
 
